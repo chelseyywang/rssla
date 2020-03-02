d84dc5f3d11fbcfcb55691a2aa81c04ffb060b4d
LilypadCollector <dc042501@gmail.com>
2020-03-02 00:36:14 -0800

    Added Contact page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Switch, Router, BrowserRouter, HashRouter} from 'react-router-dom';
-import './App.css'; 
-import Header from './components/Header/Header'; 
+import './App.css';
+import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Prospectives from './pages/Prospectives';  
-import Programs from './pages/Program/Programs'; 
-import About from './pages/About'; 
+import Prospectives from './pages/Prospectives';
+import Programs from './pages/Program/Programs';
+import About from './pages/About';
 import Board from './pages/Board';
-import NewHeader from './components/NewHeader/NewHeader'; 
+import NewHeader from './components/NewHeader/NewHeader';
+import Contact from './pages/Contact'
 
 function AppRouter() {
   return (
@@ -19,9 +20,10 @@ function AppRouter() {
         <Route path='/prospectives' component={Prospectives} />
         <Route path='/board' component={Board} />
         <Route path='/programs' component={Programs} />
+        <Route path='/contact' component={Contact} />
       </Switch>
       <Footer/>
-      </BrowserRouter> 
+      </BrowserRouter>
     </div>
   );
 }
