198a282d104b1eca13e6b021c37eea09a6df06cf
rishisankar <rishi.sankar@gmail.com>
2020-05-14 23:13:23 -0700

    Updated contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Contact.css';
-import ReactContactForm from 'react-mail-form';
+//import ReactContactForm from 'react-mail-form';
 
 function Contact() {
   return (
     <div id="contact-page">
       <p id="text">Contact Us</p>
       <p id="contact-description">Questions? Get in touch with us here!</p>
-      <ReactContactForm titlePlaceholder="Subject" contentsPlaceholder="Body" buttonText="Submit" className="contact-form" to="president@rssla.org" />
+      {/* <ReactContactForm titlePlaceholder="Subject" contentsPlaceholder="Body" buttonText="Submit" className="contact-form" to="president@rssla.org" /> */}
+      <div class="contact-form">
+        <input class="contact-field" type="email" maxlength="50" placeholder="Subject"/>
+        <textarea class="contact-field body" rows="8" maxlength="500" placeholder="Body"/>
+        <a href="mailto:president@rssla.org?subject=&amp;body=" target="_blank" rel="noopener noreferrer">Submit</a>
+      </div>
     </div>
   );
 }
